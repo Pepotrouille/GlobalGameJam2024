3f18686717664a44ab91b7798750120235d5e167
Pepotrouille <mechaneka83@gmail.com>
2024-01-27 17:46:20 +0100

    feat: add game mechanic + small fixes

--- Scripts/clickable_object.gd
+++ Scripts/clickable_object.gd
@@ -1,37 +1,15 @@
 extends Area2D
 
-
-static var hovered_object
+class_name clickable_object
 
 var is_hovered = false
 
 @export var type = Game.TYPE_OBJECTS.FLEUR
 
+func get_type():
+	return type
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_mouse_entered():
-	print("Entered")
-	is_hovered = true
-	hovered_object = self
-	print(self)
-
-func _on_mouse_exited():
-	print("Exited")
-	is_hovered = false
-	if hovered_object == self:
-		hovered_object = null
-
-func _is_hovered():
-	return is_hovered
 	
-static func what_is_hovered():
-	return hovered_object
+	
 
 
